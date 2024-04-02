fc31764330ce2ee7ef461e22cb66181391050470
Zihao Zhao <zzh_021111@sjtu.edu.cn>
2024-04-02 01:27:58 -0700

    Update PID controller parameters and add velocity control

--- src/cylinder_robot/script/controller.py
+++ src/cylinder_robot/script/controller.py
@@ -70,8 +70,11 @@ class Controller:
         self.name = 'cylinderRobot'
         rospy.Subscriber('/robot/esti_model_state', ModelState, self.callback_state)
         self.pub = rospy.Publisher("/robot/control", Twist, queue_size=10)
-        self.pid_x = PID_Controller(1, 0, 0.01, -0.6, 0.6)
-        self.pid_y = PID_Controller(1, 0, 0.01, -0.6, 0.6)
+        self.state = ModelState()
+        self.pid_x = PID_Controller(1.3, 0, 0.4, -0.6, 0.6)
+        self.pid_y = PID_Controller(1.3, 0, 0.4, -0.6, 0.6)
+        self.pid_vx = PID_Controller(10, 0, 0.01, -18, 18)
+        self.pid_vy = PID_Controller(10, 0, 0.01, -18, 18)
         self.px = 0
         self.py = 0
         self.targets_x = [1, 2, 0]
@@ -91,26 +94,40 @@ class Controller:
         return error_x, error_y
     
 
+
     def control(self):
+        twist = Twist()
         error_x, error_y = self.calculate_error()
         
+        if self.target_index == self.target_max_index:
+            rospy.loginfo('Finish')
+            return
+
         if abs(error_x) < self.tolerance and abs(error_y) < self.tolerance:
-            if self.target_index == self.target_max_index:
-                return
 
             self.target_index += 1
             self.pid_x.reset()
             self.pid_y.reset()
-            # print('success, target index/total index:', self.target_index, "/" ,self.target_max_index)
+            self.pid_vx.reset()
+            self.pid_vy.reset()
+            twist.linear.x = 0
+            twist.angular.z = 0
+            rospy.loginfo('target_index: %d', self.target_index)
+        else:
+            tmp_x = self.pid_x.get_output(error_x)
+            tmp_y = self.pid_y.get_output(error_y)
 
-        force_x = self.pid_x.get_output(error_x)
-        force_y = self.pid_y.get_output(error_y)
+            error_vx = tmp_x - self.state.twist.linear.x
+            error_vy = tmp_y - self.state.twist.angular.z
 
-        twist = Twist()
-        twist.linear.x = force_x
-        twist.angular.z = force_y
+            force_vx = self.pid_vx.get_output(error_vx)
+            force_vy = self.pid_vy.get_output(error_vy)
+
+            twist.linear.x = force_vx
+            twist.angular.z = force_vy
 
         self.pub.publish(twist)
+        rospy.loginfo('error_x: %f, error_y: %f, twist.linear.x: %f, twist.angular.z: %f', error_x, error_y, twist.linear.x, twist.angular.z)
         
         
 
